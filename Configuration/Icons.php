8341e3056878c9cd2e6a919e644f666cfb461712
janbager <jan.bager@gmail.com>
2025-09-29 10:11:53 +0200

    feat: add Featured Publications slider with backend configuration and localization

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -13,4 +13,12 @@ return [
         'provider' => SvgIconProvider::class,
         'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/project.svg',
     ],
+    'tx-hio_typo3_connector_wtl-featured-publications' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/list-of-publications.svg',
+    ],
+    'tx-hio_typo3_connector_wtl-featured-publication' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:hio_typo3_connector/Resources/Public/Icons/publication.svg',
+    ],
 ];
