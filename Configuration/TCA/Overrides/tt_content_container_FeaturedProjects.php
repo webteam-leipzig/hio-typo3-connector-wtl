a314c0134c4c216b80fe4146dffa08d1265adb99
Steffen Eißmann <steffen.eissmann@wtl.de>
2026-09-08 12:02:54 +0200

    [TASK] Add defender-ish config for typo3 14 (#25)

--- Configuration/TCA/Overrides/tt_content_container_FeaturedProjects.php
+++ Configuration/TCA/Overrides/tt_content_container_FeaturedProjects.php
@@ -5,41 +5,41 @@ declare(strict_types=1);
 use B13\Container\Tca\ContainerConfiguration;
 use B13\Container\Tca\Registry;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Information\Typo3Version;
 
 if (! defined('TYPO3')) {
     die('Access denied.');
 }
+$lllPrefix = 'LLL:EXT:hio_typo3_connector_wtl/Resources/Private/Language/locallang_be.xlf:hio.';
 
-(static function (Registry $containerRegistry): void {
-    $lllPrefix = 'LLL:EXT:hio_typo3_connector_wtl/Resources/Private/Language/locallang_be.xlf:hio.';
+$typo3Version = GeneralUtility::makeInstance(Typo3Version::class);
 
-    $containerRegistry->configureContainer(
-        (new ContainerConfiguration(
-            'tx_hiotypo3connectorwtl_featured_projects',
-            $lllPrefix . 'featuredProjects.title',
-            $lllPrefix . 'featuredProjects.description',
-            [
-                [
-                    [
-                        'name' => $lllPrefix . 'featuredProjects.contentColumn',
-                        'colPos' => 101,
-                        'allowed' => [
-                            'CType' => 'tx_hiotypo3connectorwtl_featured_project',
-                        ],
-                        'maxitems' => 12,
-                    ],
-                ],
-            ]
-        ))
+$gridConfiguration = [
+    [
+        [
+            'name' => $lllPrefix . 'featuredProjects.contentColumn',
+            'colPos' => 101,
+            'maxitems' => 12,
+        ],
+    ],
+];
+
+if (version_compare($typo3Version->getBranch(), '14.0', '>=')) {
+    $gridConfiguration[0][0]['allowedContentTypes'] = 'tx_hiotypo3connectorwtl_featured_project';
+} else {
+    $gridConfiguration[0][0]['allowed'] = [
+        'CType' => 'tx_hiotypo3connectorwtl_featured_project',
+    ];
+}
+
+GeneralUtility::makeInstance(Registry::class)->configureContainer(
+    (new ContainerConfiguration(
+        'tx_hiotypo3connectorwtl_featured_projects',
+        $lllPrefix . 'featuredProjects.title',
+        $lllPrefix . 'featuredProjects.description',
+        $gridConfiguration
+    ))
         ->setIcon('tx-hio_typo3_connector_wtl-featured-projects')
         ->setSaveAndCloseInNewContentElementWizard(false)
         ->setGroup('hio-publisher')
-    );
-
-    $GLOBALS['TCA']['tt_content']['types']['tx_hiotypo3connectorwtl_featured_projects']['showitem'] = str_replace(
-        'header;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header.ALT.div_formlabel,',
-        '--palette--;;headers,',
-        (string)$GLOBALS['TCA']['tt_content']['types']['tx_hiotypo3connectorwtl_featured_projects']['showitem']
-    );
-    $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['tx_hiotypo3connectorwtl_featured_projects'] = 'tx-hio_typo3_connector_wtl-featured-projects';
-})(GeneralUtility::makeInstance(Registry::class));
+);
--- Configuration/TCA/Overrides/tt_content_container_FeaturedPublications.php
+++ Configuration/TCA/Overrides/tt_content_container_FeaturedPublications.php
@@ -5,41 +5,41 @@ declare(strict_types=1);
 use B13\Container\Tca\ContainerConfiguration;
 use B13\Container\Tca\Registry;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Information\Typo3Version;
 
 if (! defined('TYPO3')) {
     die('Access denied.');
 }
+$lllPrefix = 'LLL:EXT:hio_typo3_connector_wtl/Resources/Private/Language/locallang_be.xlf:hio.';
 
-(static function (Registry $containerRegistry): void {
-    $lllPrefix = 'LLL:EXT:hio_typo3_connector_wtl/Resources/Private/Language/locallang_be.xlf:hio.';
+$typo3Version = GeneralUtility::makeInstance(Typo3Version::class);
 
-    $containerRegistry->configureContainer(
-        (new ContainerConfiguration(
-            'tx_hiotypo3connectorwtl_featured_publications',
-            $lllPrefix . 'featuredPublications.title',
-            $lllPrefix . 'featuredPublications.description',
-            [
-                [
-                    [
-                        'name' => $lllPrefix . 'featuredPublications.contentColumn',
-                        'colPos' => 111,
-                        'allowed' => [
-                            'CType' => 'tx_hiotypo3connectorwtl_featured_publication',
-                        ],
-                        'maxitems' => 12,
-                    ],
-                ],
-            ]
-        ))
+$gridConfiguration = [
+    [
+        [
+            'name' => $lllPrefix . 'featuredPublications.contentColumn',
+            'colPos' => 111,
+            'maxitems' => 12,
+        ],
+    ],
+];
+
+if (version_compare($typo3Version->getBranch(), '14.0', '>=')) {
+    $gridConfiguration[0][0]['allowedContentTypes'] = 'tx_hiotypo3connectorwtl_featured_publication';
+} else {
+    $gridConfiguration[0][0]['allowed'] = [
+        'CType' => 'tx_hiotypo3connectorwtl_featured_publication',
+    ];
+}
+
+GeneralUtility::makeInstance(Registry::class)->configureContainer(
+    (new ContainerConfiguration(
+        'tx_hiotypo3connectorwtl_featured_publications',
+        $lllPrefix . 'featuredPublications.title',
+        $lllPrefix . 'featuredPublications.description',
+        $gridConfiguration
+    ))
         ->setIcon('tx-hio_typo3_connector_wtl-featured-publications')
         ->setSaveAndCloseInNewContentElementWizard(false)
         ->setGroup('hio-publisher')
-    );
-
-    $GLOBALS['TCA']['tt_content']['types']['tx_hiotypo3connectorwtl_featured_publications']['showitem'] = str_replace(
-        'header;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:header.ALT.div_formlabel,',
-        '--palette--;;headers,',
-        (string)$GLOBALS['TCA']['tt_content']['types']['tx_hiotypo3connectorwtl_featured_publications']['showitem']
-    );
-    $GLOBALS['TCA']['tt_content']['ctrl']['typeicon_classes']['tx_hiotypo3connectorwtl_featured_publications'] = 'tx-hio_typo3_connector_wtl-featured-publications';
-})(GeneralUtility::makeInstance(Registry::class));
+);
